65d0f8ae43dedec783d7fa5e4298d25cfa59f902
LapisThe <inskepticskeptic@gmail.com>
2024-08-16 10:27:03 +0700

    Fix sizing for smaller devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 			>
 				psst... hover over me :3
 			</p>
-			<p className={"absolute text-center text-base sm:bottom-6"}>
+			<p className={"absolute text-center text-base md:bottom-6"}>
 				<i>
 					pfp by <Link href="https://twitter.com/etunisgood2">etun</Link>!!
 				</i>
@@ -25,27 +25,29 @@ export default function Home() {
 				onMouseLeave={() => setHover(false)}
 			>
 				<Image
-					className={`relative z-10 top-72 w-60 h-60 ${hover ? "scale-90 -translate-y-32" : "scale-100 translate-y-0"} rounded-lg transition duration-1000 ease-out`}
+					className={`relative z-10 top-72 w-60 h-60 object-cover ${hover ? "scale-90 -translate-y-32" : "scale-100 translate-y-0"} rounded-lg transition duration-1000 ease-out`}
 					src="/lapis_transparent.png"
 					width={500}
 					height={500}
 					alt="me"
 				/>
 				<div
-					className={`absolute flex flex-col top-96 gap-2 p-8 items-center text-center text-xl w-full ${hover ? "translate-y-0 opacity-100" : "-translate-y-8 opacity-0"} transition duration-1000 ease-out`}
+					className={`absolute flex flex-col top-96 gap-2 p-8 items-center text-center text-base md:text-xl w-full ${hover ? "translate-y-0 opacity-100" : "-translate-y-8 opacity-0"} transition duration-1000 ease-out`}
 				>
-					<p className="text-2xl">
+					<p className="text-xl md:text-2xl">
 						<b>LapisWolf</b>
 					</p>
-					<p className="text-base">@lapisw</p>
+					<p className="text-sm md:text-base">@lapisw</p>
 					<p className="text-xs">📌 Seattle, WA</p>
-					<hr className={`${hover ? "w-52" : "w-0"} transition-all duration-1000 ease-out`} />
+					<hr className={`${hover ? "w-48 md:w-52" : "w-0"} transition-all duration-1000 ease-out`} />
 					<p>
 						👋 Hiya, I&apos;m Lapis!
 						<br />I absolutely love planes, develop software as a side hobby, and <i>occassionally</i> work
 						on Roblox games!
 					</p>
-					<hr className={`${hover ? "w-96" : "w-0"} transition-all delay-200 duration-1000 ease-out`} />
+					<hr
+						className={`${hover ? "w-80 md:w-96" : "w-0"} transition-all delay-200 duration-1000 ease-out`}
+					/>
 					<p>See me on...</p>
 					<Link href="https://github.com/LapisThe">GitHub</Link>
 					<Link href="https://steamcommunity.com/id/lapisyuh">Steam</Link>
