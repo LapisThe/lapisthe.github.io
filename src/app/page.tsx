0aea1a10df4aebe17e40f9d4d37d6ab36335c2e1
LapisThe <inskepticskeptic@gmail.com>
2024-10-14 20:03:57 -0700

    Fix mobile responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,29 +12,29 @@ export default function Home() {
 		<BackgroundBeamsWithCollision>
 			<main className="flex h-screen w-screen flex-col items-center p-6">
 				<p
-					className={`relative text-center text-sm top-32 md:top-64 ${hover ? "opacity-0" : "opacity-100"} transition duration-300 ease-out`}
+					className={`relative text-center text-sm top-64 ${hover ? "opacity-0" : "opacity-100"} transition duration-300 ease-out`}
 				>
 					psst... hover over me :3
 				</p>
-				<p className={"absolute text-center text-base md:bottom-6"}>
+				<p className={"absolute text-center text-sm md:text-base"}>
 					<i>
 						pfp by <Link href="https://twitter.com/etunisgood2">etun</Link>!!
 					</i>
 				</p>
 				<div
-					className="relative flex flex-col items-center mx-auto w-full md:w-1/2 h-1/2"
+					className="relative flex flex-col mt-72 items-center mx-auto w-full md:w-1/2 h-1/2"
 					onMouseEnter={() => setHover(true)}
 					onMouseLeave={() => setHover(false)}
 				>
 					<Image
-						className={`relative z-10 top-48 md:top-72 w-60 h-60 object-cover ${hover ? "scale-90 -translate-y-32" : "scale-100 translate-y-0"} rounded-xl transition duration-1000 ease-out`}
+						className={`relative z-10 w-60 h-auto object-cover ${hover ? "scale-90 -translate-y-32" : "scale-100 translate-y-0"} rounded-xl transition duration-1000 ease-out`}
 						src="/lapis_transparent.png"
 						width={500}
 						height={500}
 						alt="me"
 					/>
 					<div
-						className={`absolute flex flex-col top-72 md:top-96 gap-2 p-8 items-center text-center text-base md:text-xl w-full ${hover ? "translate-y-0 opacity-100" : "-translate-y-8 opacity-0"} transition duration-1000 ease-out`}
+						className={`relative flex flex-col bottom-28 gap-2 items-center text-center text-base md:text-xl w-full ${hover ? "translate-y-0 opacity-100" : "-translate-y-8 opacity-0"} transition duration-1000 ease-out`}
 					>
 						<p className="text-xl md:text-2xl">
 							<b>LapisWolf</b>
