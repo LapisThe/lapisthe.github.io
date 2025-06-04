fca4354536552cdd91ffe3c20f84b7c473876aa5
lapisthe <87749912+LapisThe@users.noreply.github.com>
2025-06-03 17:09:37 -0700

    Fix justify positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,22 +12,22 @@ export default function Home() {
 
 	return (
 		<BackgroundBeamsWithCollision>
-			<main className="flex h-screen w-screen flex-col justify-center items-center gap-6 p-6">
-				<p
-					className={`text-center text-sm md:text-base ${hover ? "opacity-0" : "opacity-100"} transition duration-300 ease-out`}
-				>
-					Psst... Hover over me!
-				</p>
+			<main className="flex h-screen w-screen flex-col justify-center items-center p-8">
 				<p className={"absolute top-4 text-center text-sm md:text-base"}>
 					<i>
 						Profile pic. by <Link href="https://twitter.com/etunisgood2">Etun</Link>
 					</i>
 				</p>
 				<div
-					className="flex flex-col items-center justify-center w-full md:w-1/2 h-1/2"
+					className="flex flex-col items-center w-full md:w-1/2 h-1/2"
 					onMouseEnter={() => setHover(true)}
 					onMouseLeave={() => setHover(false)}
 				>
+					<p
+						className={`p-4 text-center text-sm md:text-base ${hover ? "opacity-0" : "opacity-100"} transition duration-300 ease-out`}
+					>
+						Psst... Hover over me!
+					</p>
 					<Image
 						className={`z-10 w-60 h-auto object-cover ${hover ? "scale-90 -translate-y-32" : "scale-100 translate-y-0"} rounded-xl transition duration-1000 ease-out`}
 						src="/lapis_transparent.png"
@@ -36,7 +36,7 @@ export default function Home() {
 						alt="me"
 					/>
 					<div
-						className={`relative flex flex-col bottom-28 gap-2 items-center text-center text-base/8 md:text-xl/8 w-full md:w-2/3 ${hover ? "translate-y-0 opacity-100" : "-translate-y-8 opacity-0"} transition duration-1000 ease-out`}
+						className={`relative flex flex-col bottom-28 gap-2 items-center text-center text-xl/8 w-full md:w-2/3 ${hover ? "translate-y-0 opacity-100" : "-translate-y-8 opacity-0"} transition duration-1000 ease-out`}
 					>
 						<p className="text-xl md:text-2xl">
 							<b>Lapis Wolf</b>
