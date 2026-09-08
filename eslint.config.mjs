ce8baea4c7c1fbf2cf30fba2da07f280866235f6
Lapis! <87749912+LapisThe@users.noreply.github.com>
2026-09-08 18:25:29 +0700

    Update vite & packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import reactHooks from "eslint-plugin-react-hooks";
@@ -9,12 +8,7 @@ export default defineConfig([
 	globalIgnores(["dist"]),
 	{
 		files: ["**/*.{ts,tsx}"],
-		extends: [
-			js.configs.recommended,
-			tseslint.configs.recommended,
-			reactHooks.configs.flat.recommended,
-			reactRefresh.configs.vite,
-		],
+		extends: [tseslint.configs.recommended, reactHooks.configs.flat.recommended, reactRefresh.configs.vite],
 		languageOptions: {
 			ecmaVersion: 2020,
 			globals: globals.browser,
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [react(), tailwindcss(), tsconfigPaths()],
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		tsconfigPaths: true,
+	},
 });
